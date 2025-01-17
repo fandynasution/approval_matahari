97a17a8f133b2760b0eec49cc6355f29a4050420
fandynasution <ahmad.ariffandy@ifca.co.id>
2025-01-17 10:21:26 +0700

    Change Title

--- resources/views/email/cmentry/send.blade.php
+++ resources/views/email/cmentry/send.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Type" content="application/pdf">
     <meta name="x-apple-disable-message-reformatting">
-    <title>IFCA - BTID</title>
+    <title>IFCA - Matahari Land</title>
     <link rel="shortcut icon" type="image/x-icon" href="{{ url('public/images/KuraKuraBali-iconew.ico') }}">
     
     <style>
