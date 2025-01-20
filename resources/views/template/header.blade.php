beb931e3cb728635632d2fe119c920a388c92ff6
fandynasution <ahmad.ariffandy@ifca.co.id>
2025-01-20 16:31:18 +0700

    Header

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -1,8 +1,8 @@
 <tbody>
     <tr>
         <td style="text-align: center; padding-bottom:25px;color: #000000 !important;">
-            <img width = "120" src="{{ url('/public/images/header.png') }}" alt="logo">
-            <p style="font-size: 16px; color: #000000; padding-top: 0px;">PT. Suryacipta Swadaya</p>
+            <img width = "120" src="{{ url('public/images/email_header.jpg') }}" alt="logo">
+            <p style="font-size: 16px; color: #000000; padding-top: 0px;">PT MENTARI AGUNG MANDIRI</p>
         </td>
     </tr>
 </tbody>
