0a926e8ae80f07dbfa0ef4bd28f5f56281c11795
fandynasution <ahmad.ariffandy@ifca.co.id>
2025-01-21 13:00:17 +0700

    Land Submission

--- app/Mail/LandSubmissionEmail.php
+++ app/Mail/LandSubmissionEmail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SendPoMail extends Mailable
+class LandSubmissionEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
